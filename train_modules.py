a6d6601fe5ebabd5d2a328c65875742446dafb28
saharsamr <sahar.rajabi76@gmail.com>
2024-07-27 13:55:22 +0330

    fix minor bug

--- train_modules.py
+++ train_modules.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     training_arguments = TrainingArguments(
         output_dir=args.output_dir,
-        num_train_epochs=args.epoch,
+        num_train_epochs=args.epochs,
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
         gradient_accumulation_steps=args.gradient_accumulation,
