6c4d81d0bca8c6ed822aa35237dfd4e86e3e6939
saharsamr <sahar.rajabi76@gmail.com>
2024-08-08 14:07:21 +0330

    fix minor bugs

--- test_modules.py
+++ test_modules.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             top_k=3,
             top_p=0.95
     )
-    label = input_ids.batch_decode(input_ids, skip_special_tokens=True)[0].split('Output:')[-1]
+    label = tokenizer.batch_decode(input_ids, skip_special_tokens=True)[0].split('Output:')[-1]
     print('-'*100)
     print(label)
     print('-'*100)
