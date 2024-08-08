a6f1f97ad438ec2b5b328f56e68e1f41d59855e4
saharsamr <sahar.rajabi76@gmail.com>
2024-08-08 18:17:46 +0330

    change some parameters

--- test_modules.py
+++ test_modules.py
@@ -55,16 +55,9 @@ if __name__ == "__main__":
                 input_ids=input_ids,
                 eos_token_id=tokenizer.eos_token_id,
                 max_new_tokens=100,
-                do_sample=True,
-                repetition_penalty=2.0,
-                temperature=0.7,
-                top_k=3,
-                top_p=0.95
         )
         labels = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
         outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
         labels = [label.split('### Response:')[-1] for label in labels]
         outputs = [output.split('### Response:')[-1] for output in outputs]
-
-        print(compute_experts_metrics(outputs, outputs))
