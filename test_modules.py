df25baeca7538cdea8459b2716f0819d2657420a
saharsamr <sahar.rajabi76@gmail.com>
2024-08-08 19:03:11 +0330

    test

--- test_modules.py
+++ test_modules.py
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     args = test_arg_parser()
     set_seed(args.seed)
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint_path)
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint_path, use_fast=True)
     quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
@@ -38,6 +37,7 @@ if __name__ == "__main__":
     )
     model = AutoModelForCausalLM.from_pretrained(
         args.model_checkpoint_path,
+        torch_dtype=torch.float16,
         quantization_config=quantization_config,
         device_map="auto"
     )
@@ -45,42 +45,23 @@ if __name__ == "__main__":
     test_ds = read_dataset(args.dataset_name, args.cluster_idx, args.data_portion, return_test=True)
     test_dataloader = DataLoader(test_ds, batch_size=args.batch_size)
 
-    # with torch.no_grad():
-    #     for batch in tqdm(test_dataloader):
-    #
-    #         input_ids = tokenizer(
-    #             batch['source'], padding='max_length', truncation=True,
-    #             return_tensors='pt',
-    #             max_length=MAX_SOURCE_TOKENS).input_ids.to("cuda")
-    #         outputs = model.generate(
-    #                 input_ids=input_ids,
-    #                 eos_token_id=tokenizer.eos_token_id,
-    #                 pad_token_id=tokenizer.eos_token_id,
-    #                 max_new_tokens=100,
-    #         )
-    #         labels = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-    #         outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    #
-    #         labels = [label.split('### Response:')[-1] for label in labels]
-    #         outputs = [output.split('### Response:')[-1] for output in outputs]
-    #
-    #         print(compute_experts_metrics(outputs, outputs))
-
     with torch.no_grad():
-        input_ids = tokenizer(
-            test_dataloader['source'], padding='max_length', truncation=True,
-            return_tensors='pt',
-            max_length=MAX_SOURCE_TOKENS).input_ids.to("cuda")
-        outputs = model.generate(
-            input_ids=input_ids,
-            eos_token_id=tokenizer.eos_token_id,
-            pad_token_id=tokenizer.eos_token_id,
-            max_new_tokens=100,
-        )
-        labels = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-        outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        for batch in tqdm(test_dataloader):
 
-        labels = [label.split('### Response:')[-1] for label in labels]
-        outputs = [output.split('### Response:')[-1] for output in outputs]
+            input_ids = tokenizer(
+                batch['source'], padding='max_length', truncation=True,
+                return_tensors='pt',
+                max_length=MAX_SOURCE_TOKENS).input_ids.to("cuda")
+            outputs = model.generate(
+                    input_ids=input_ids,
+                    eos_token_id=tokenizer.eos_token_id,
+                    pad_token_id=tokenizer.eos_token_id,
+                    max_new_tokens=100,
+            )
+            labels = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+            outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
-        print(compute_experts_metrics(outputs, outputs))
+            labels = [label.split('### Response:')[-1] for label in labels]
+            outputs = [output.split('### Response:')[-1] for output in outputs]
+
+            print(compute_experts_metrics(outputs, outputs))
