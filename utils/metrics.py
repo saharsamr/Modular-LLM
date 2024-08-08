51f0d7f687c1c67fb13fd818c8c1bd0c456cae9b
saharsamr <sahar.rajabi76@gmail.com>
2024-08-08 16:47:50 +0330

    fix minor bugs

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,20 +1,21 @@
+from nltk.tokenize import word_tokenize
 from datasets import load_metric
-import numpy as np
 
 
 def compute_experts_metrics(labels, predictions):
-    print(labels)
-    print(predictions)
     bleu_metric = load_metric('bleu', trust_remote_code=True)
     rouge_metric = load_metric('rouge', trust_remote_code=True)
     bert_score = load_metric('bertscore', trust_remote_code=True)
 
+    references = [[word_tokenize(ref)] for ref in labels]
+    predictions = [word_tokenize(pred) for pred in predictions]
+
     bleu_output = bleu_metric.compute(
-        predictions=predictions, references=labels, max_order=1)
+        predictions=predictions, references=references, max_order=1)
     rouge_output = rouge_metric.compute(
-        predictions=predictions, references=labels, rouge_types=['rougeL'])
+        predictions=predictions, references=references, rouge_types=['rougeL'])
     bertscore_output = bert_score.compute(
-        predictions=predictions, references=labels, lang='en', model_type='bert-base-uncased')
+        predictions=predictions, references=references, lang='en', model_type='bert-base-uncased')
 
     return {
         'bleu': bleu_output,
