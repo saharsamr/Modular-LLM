de46e0d59246d25d45acd0c57bacd315f5b936b9
TheTahaaa <m.taha77@me.com>
2024-08-25 19:22:35 +0000

    Adding xlora merging method, training method should be completed.

---
+++ merging_lora_modules/xlora_average.py
@@ -0,0 +1,41 @@
+from merging_lora_modules.base_merging_module import (
+    cluster_checkpoint_names,
+)
+from transformers import AutoConfig
+
+import xlora
+import torch
+
+
+class XLoraAveraging():
+    def __init__(self, base_model, model_name):
+        self.base_model = base_model
+        self.model_name = model_name
+        self.base_model_config = AutoConfig.from_pretrained(
+                                    model_name,
+                                    trust_remote_code=True,
+                                    use_flash_attention_2=False,
+                                    device_map="auto",
+                                )
+        
+
+    def create_model(self):
+        self.model = xlora.add_xlora_to_model(
+                            model=self.base_model,
+                            xlora_config=xlora.xLoRAConfig(
+                                self.base_model_config.hidden_size,
+                                base_model_id=self.model_name,
+                                xlora_depth=4,
+                                device=torch.device("cuda"),
+                                adapters=cluster_checkpoint_names,
+                            ),
+                            verbose=True,
+                        )
+
+
+    def train(self):
+        ## TODO: Writing the training code for the xlora_model, similar to module_trainer.train() method
+
+        
+
+    
