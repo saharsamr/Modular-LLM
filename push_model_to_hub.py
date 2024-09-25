834adf0158fc867105b5069aea317db9b77203f4
TheTahaaa <m.taha77@me.com>
2024-09-25 07:59:25 +0000

    Push experts to the huggingface hub.

---
+++ push_model_to_hub.py
@@ -0,0 +1,14 @@
+from merging_lora_modules.base_merging_module import cluster_checkpoint_names
+from huggingface_hub import HfApi
+
+
+if __name__ == "__main__":
+    api = HfApi()
+
+    for model_ckpt_name, model_ckpt_path in cluster_checkpoint_names.items():
+        api.upload_folder(
+            folder_path=model_ckpt_path,
+            repo_id="AliEdalat/experts",
+            path_in_repo=model_ckpt_name,
+            token='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',
+        )
