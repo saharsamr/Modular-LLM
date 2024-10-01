49b90ffd8247d72f4b9f32c192ca3680097f14d8
Ali Edalat <ali.edalat.77@gmail.com>
2024-10-02 01:33:05 +0330

    set adapter in layers arrow merging

--- merging_lora_modules/arrow_routing.py
+++ merging_lora_modules/arrow_routing.py
@@ -5,124 +5,95 @@ from tensordict.tensordict import TensorDict
 from torch import nn
 from peft import PeftModel
 
+class CustomModel(nn.Module):
+    def __init__(self, base_model, cluster_checkpoint_names, experts_prototypes, k=1):
+        super().__init__()
+        # Load the base model with the first cluster (default LoRA adapter)
+        self.base_model = PeftModel.from_pretrained(
+            base_model, cluster_checkpoint_names['cluster0'], adapter_name='cluster0')
+        
+        # Load additional experts (LoRA adapters)
+        for cluster_name, cluster_path in cluster_checkpoint_names.items():
+            if cluster_name != 'cluster0':
+                self.base_model.load_adapter(cluster_path, adapter_name=cluster_name)
+        
+        # Store the expert mapping for each layer
+        self.experts_prototypes = experts_prototypes
+        self.k = k
+
+    def expert_mapping(self, layer_index, current_input):
+
+        logits = {}
+        for key in experts_prototypes[layer_index].keys():
+            logits[key] = torch.abs(torch.dot(experts_prototypes[layer_index][key], current_input))
+
+        # Sort the dictionary by values and get the top k items
+        top_k = sorted(logits.items(), key=lambda item: item[1], reverse=True)[:self.k]
+
+        # Extract keys and values from top_k
+        top_k_keys = [item[0] for item in top_k]
+
+        return top_k_keys
+        
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+        # Initial input is the input_ids from the text
+        current_input = input_ids
+        
+        # Iterate through each layer and apply the corresponding expert
+        for layer_index, layer in enumerate(self.base_model.model.layers):
+            # Select the expert based on the mapping for the current layer
+            expert_index = self.expert_mapping(layer_index, current_input)
+
+            # Switch to the specific LoRA expert for this layer
+            self.base_model.set_adapter(f'cluster{expert_index}')
+
+            # Perform the forward pass for this layer using the current input
+            output = layer(current_input, attention_mask=attention_mask, token_type_ids=token_type_ids)
+
+            # Update the input for the next layer to be the output of the current layer
+            current_input = output
+
+        # Return the final output after passing through all layers
+        return current_input
+
 
 class CustomPeftModel(PeftModel):
     def __init__(self, base_model, peft_config):
         super().__init__(base_model, peft_config)
 
+    def set_layer_expert():
+        pass
 
-    def _low_rank_svd(self, A, B):
-        """Faster SVD computation for low rank matrices"""
+    def forward(self, *args, **kwargs):
+        # Iterate through each layer and apply the corresponding expert
+        for layer_index, layer in enumerate(self.base_model.model.layers):
+            # Select the expert based on the mapping for the current layer
+            expert_index = self.expert_mapping[layer_index]
 
-        # Compute SVD of A
-        U_A, Sigma_A, V_A = torch.svd(A)
+            # Switch to the specific LoRA expert for this layer
+            self.base_model.set_adapter(f'cluster{expert_index}')
 
-        # Compute SVD of B.T (transpose of B)
-        U_B, Sigma_B, V_B = torch.svd(B.T)
-
-        # Compute product matrix C = Sigma_A * (V_A.T @ V_B) * Sigma_B
-        # Since V_A and V_B are orthogonal, their product is also an orthogonal matrix
-        C = Sigma_A.diag_embed() @ V_A.t() @ V_B @ Sigma_B.diag_embed()
-
-        # Compute SVD of the product matrix C
-        U_C, Sigma_C, V_C = torch.svd(C)
-
-        # Construct the final SVD components of W
-        U_W = U_A @ U_C
-        V_W_T = V_C.t() @ U_B.t()
-
-        diff_AB = (U_W.T @ U_A).abs().diag()
-        if diff_AB[0] < 0.9:
-            print("The first singular vector of U_A and U_AB are not aligned")
-
-        return U_W, Sigma_C, V_W_T
-
-
-    def forward(self, input_ids, k, *args, **kwargs):
+            # Perform the forward pass for this layer
+            output = layer(*args, **kwargs)
+            
+            # Process the output (you may need to modify this based on your model structure)
+            # Example: you may want to store or process the output here
         
-        # TODO: Modify the forward pass to iterate on the layers and apply the selected prototype
-        for module in self.base_model.modules(): 
-            if isinstance(module, LoraLayer): 
-                if proj_counter % 2 == 0:
-                    # module is o_proj
-                    o_proj_lora_A_module_dict = module.lora_A
-                    o_proj_lora_B_module_dict = module.lora_B
-                else:
-                    # module is qkv_proj
-                    qkv_proj_lora_A_module_dict = module.lora_A
-                    qkv_proj_lora_B_module_dict = module.lora_B
-
-                    # Now we concat o_proj and qkv_proj on dim = 1
-                    concat_lora_A_dict = TensorDict()
-                    concat_lora_B_dict = TensorDict()
-
-                    for adapter_name in cluster_checkpoint_names.keys():
-                        o_proj_lora_A = o_proj_lora_A_module_dict[adapter_name].weight.T
-                        o_proj_lora_B = o_proj_lora_B_module_dict[adapter_name].weight
-
-                        qkv_proj_lora_A = qkv_proj_lora_A_module_dict[adapter_name].weight.T
-                        qkv_proj_lora_B = qkv_proj_lora_B_module_dict[adapter_name].weight
-
-                        expert_lora_A = torch.cat((o_proj_lora_A, qkv_proj_lora_A), dim=0)
-                        expert_lora_B = torch.cat((o_proj_lora_B, qkv_proj_lora_B), dim=0).T
-
-                        concat_lora_A_dict[adapter_name] = expert_lora_A
-                        concat_lora_B_dict[adapter_name] = expert_lora_B
-                    
-                    # Now we add both lora_A and lora_B dictionaries to the global dictionary
-                    ABs = {'lora_A': concat_lora_A_dict, 'lora_B': concat_lora_B_dict}
-
-                    vectors_dict = {}
-                    eigvals_dict = {}
-                    scores = {}
-                    # cumpute prototype of experts in the current layer
-                    for cluster in ABs['lora_A'].keys():
-
-                        A = ABs['lora_A'][cluster]
-                        # A = [tensor.tolist() for tensor in A.values()]
-
-                        B = ABs['lora_B'][cluster]
-
-                        W = (A @ B).T  # out_features, in_features
-
-                        U_W, Sigma_W, _ = self._low_rank_svd(A, B)
-                        top_value = Sigma_W[0] ** 2
-                        bottom_vector = U_W[:, -1]
-                        top_vector = U_W[:, 0]
-
-                        # Check that top vector is indeed an eigenvector
-                        WTW = W.T @ W
-                        ratio = WTW @ top_vector / (top_vector * top_value)
-                        torch.allclose(ratio, torch.ones_like(ratio), atol=1e-3)
-
-                        # Check that top vector is indeed the top eigenvector
-                        assert (WTW @ top_vector).pow(2).sum() > (WTW @ bottom_vector).pow(2).sum()
-
-                        # Save eigenvector and eigvenvalue
-                        vectors_dict[cluster] = top_vector.detach().cpu().numpy()
-                        eigvals_dict[cluster] = top_value.item()
-                        scores[cluster] = top_vector.detach().cpu().numpy() * layer_input_h
-
-                    # TODO: logits based on the scores and select k-top experts. average k-top experts based on scores.
-
-                    # TODO: define output of layer based on merge of experts
-
-
-
-
-        return modified_output
+        # Continue the rest of the forward pass as usual
+        return output
 
 
 class ArrowRouting(BaseMergingModule):
     def __init__(self, base_model, tokenizer, model_name):
         super().__init__(base_model, tokenizer, model_name)
 
-    def load_lora_modules(self):
-        self.base_model = CustomPeftModel.from_pretrained(
-            self.base_model, cluster_checkpoint_names['cluster0'], adapter_name='cluster0')
-        for cluster_name, cluster_path in cluster_checkpoint_names.items():
-            if cluster_name != 'cluster0':
-                self.base_model.load_adapter(cluster_path, adapter_name=cluster_name)
+    # def load_lora_modules(self):
+    #     self.base_model = CustomPeftModel.from_pretrained(
+    #         self.base_model, cluster_checkpoint_names['cluster0'], adapter_name='cluster0')
+    #     for cluster_name, cluster_path in cluster_checkpoint_names.items():
+    #         if cluster_name != 'cluster0':
+    #             self.base_model.load_adapter(cluster_path, adapter_name=cluster_name)
         
 
     def _low_rank_svd(self, A, B):
@@ -244,9 +215,9 @@ class ArrowRouting(BaseMergingModule):
                 vectors_dict[layer][cluster] = top_vector.detach().cpu().numpy()
                 eigvals_dict[layer][cluster] = top_value.item()
 
-        print(vectors_dict, eigvals_dict)
+        # print(vectors_dict, eigvals_dict)
 
         return vectors_dict, eigvals_dict
 
     def get_model(self, vectors_dict):
-        pass
+        return CustomModel(self.base_model, cluster_checkpoint_names, vectors_dict)
