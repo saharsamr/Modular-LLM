420b65a18f21fa8fae881f2324252dcabb3754a6
AliEdalat <ali.edalat.77@gmail.com>
2024-10-01 18:00:25 +0330

    add CustomPeftModel that compute prototype and diff expert in diff layer. change forward method.

--- merging_lora_modules/arrow_routing.py
+++ merging_lora_modules/arrow_routing.py
@@ -3,11 +3,126 @@ from peft.tuners.lora.layer import LoraLayer
 import torch
 from tensordict.tensordict import TensorDict
 from torch import nn
+from peft import PeftModel
+
+
+class CustomPeftModel(PeftModel):
+    def __init__(self, base_model, peft_config):
+        super().__init__(base_model, peft_config)
+
+
+    def _low_rank_svd(self, A, B):
+        """Faster SVD computation for low rank matrices"""
+
+        # Compute SVD of A
+        U_A, Sigma_A, V_A = torch.svd(A)
+
+        # Compute SVD of B.T (transpose of B)
+        U_B, Sigma_B, V_B = torch.svd(B.T)
+
+        # Compute product matrix C = Sigma_A * (V_A.T @ V_B) * Sigma_B
+        # Since V_A and V_B are orthogonal, their product is also an orthogonal matrix
+        C = Sigma_A.diag_embed() @ V_A.t() @ V_B @ Sigma_B.diag_embed()
+
+        # Compute SVD of the product matrix C
+        U_C, Sigma_C, V_C = torch.svd(C)
+
+        # Construct the final SVD components of W
+        U_W = U_A @ U_C
+        V_W_T = V_C.t() @ U_B.t()
+
+        diff_AB = (U_W.T @ U_A).abs().diag()
+        if diff_AB[0] < 0.9:
+            print("The first singular vector of U_A and U_AB are not aligned")
+
+        return U_W, Sigma_C, V_W_T
+
+
+    def forward(self, input_ids, k, *args, **kwargs):
+        
+        # TODO: Modify the forward pass to iterate on the layers and apply the selected prototype
+        for module in self.base_model.modules(): 
+            if isinstance(module, LoraLayer): 
+                if proj_counter % 2 == 0:
+                    # module is o_proj
+                    o_proj_lora_A_module_dict = module.lora_A
+                    o_proj_lora_B_module_dict = module.lora_B
+                else:
+                    # module is qkv_proj
+                    qkv_proj_lora_A_module_dict = module.lora_A
+                    qkv_proj_lora_B_module_dict = module.lora_B
+
+                    # Now we concat o_proj and qkv_proj on dim = 1
+                    concat_lora_A_dict = TensorDict()
+                    concat_lora_B_dict = TensorDict()
+
+                    for adapter_name in cluster_checkpoint_names.keys():
+                        o_proj_lora_A = o_proj_lora_A_module_dict[adapter_name].weight.T
+                        o_proj_lora_B = o_proj_lora_B_module_dict[adapter_name].weight
+
+                        qkv_proj_lora_A = qkv_proj_lora_A_module_dict[adapter_name].weight.T
+                        qkv_proj_lora_B = qkv_proj_lora_B_module_dict[adapter_name].weight
+
+                        expert_lora_A = torch.cat((o_proj_lora_A, qkv_proj_lora_A), dim=0)
+                        expert_lora_B = torch.cat((o_proj_lora_B, qkv_proj_lora_B), dim=0).T
+
+                        concat_lora_A_dict[adapter_name] = expert_lora_A
+                        concat_lora_B_dict[adapter_name] = expert_lora_B
+                    
+                    # Now we add both lora_A and lora_B dictionaries to the global dictionary
+                    ABs = {'lora_A': concat_lora_A_dict, 'lora_B': concat_lora_B_dict}
+
+                    vectors_dict = {}
+                    eigvals_dict = {}
+                    scores = {}
+                    # cumpute prototype of experts in the current layer
+                    for cluster in ABs['lora_A'].keys():
+
+                        A = ABs['lora_A'][cluster]
+                        # A = [tensor.tolist() for tensor in A.values()]
+
+                        B = ABs['lora_B'][cluster]
+
+                        W = (A @ B).T  # out_features, in_features
+
+                        U_W, Sigma_W, _ = self._low_rank_svd(A, B)
+                        top_value = Sigma_W[0] ** 2
+                        bottom_vector = U_W[:, -1]
+                        top_vector = U_W[:, 0]
+
+                        # Check that top vector is indeed an eigenvector
+                        WTW = W.T @ W
+                        ratio = WTW @ top_vector / (top_vector * top_value)
+                        torch.allclose(ratio, torch.ones_like(ratio), atol=1e-3)
+
+                        # Check that top vector is indeed the top eigenvector
+                        assert (WTW @ top_vector).pow(2).sum() > (WTW @ bottom_vector).pow(2).sum()
+
+                        # Save eigenvector and eigvenvalue
+                        vectors_dict[cluster] = top_vector.detach().cpu().numpy()
+                        eigvals_dict[cluster] = top_value.item()
+                        scores[cluster] = top_vector.detach().cpu().numpy() * layer_input_h
+
+                    # logits based on the scores and select k-top experts. average k-top experts based on scores.
+
+                    # define output of layer based on merge of experts
+
+
+
+
+        return modified_output
 
 
 class ArrowRouting(BaseMergingModule):
     def __init__(self, base_model, tokenizer, model_name):
         super().__init__(base_model, tokenizer, model_name)
+
+    def load_lora_modules(self):
+        self.base_model = CustomPeftModel.from_pretrained(
+            self.base_model, cluster_checkpoint_names['cluster0'], adapter_name='cluster0')
+        for cluster_name, cluster_path in cluster_checkpoint_names.items():
+            if cluster_name != 'cluster0':
+                self.base_model.load_adapter(cluster_path, adapter_name=cluster_name)
         
 
     def _low_rank_svd(self, A, B):
