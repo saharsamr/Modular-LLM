43635a522f3759b5c0af7af6c45c95ec08c7c20c
saharsamr <sahar.rajabi76@gmail.com>
2024-10-26 19:11:22 +0330

    test

--- merging_modules_vram_control.py
+++ merging_modules_vram_control.py
@@ -108,14 +108,16 @@ if __name__ == "__main__":
     for i, batch in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
         # Calling the model's forward path to apply Arrow Routing
 
-        # print(batch['text'])
+        print(batch['text'])
+        print(batch['label'])
+        continue
 
         tokenised_batch = tokenizer(batch['text'], return_tensors="pt", truncation=True, padding=True).to('cuda')
 
         if len(tokenised_batch['input_ids'][0]) > 1500:
             continue
         if len(references) > 20:
-            break
+            continue
 
         if args.merging_strategy == 'arrow_routing':
             strategy_model(**tokenised_batch, compute_arrow_weights=True, top_k=3)
