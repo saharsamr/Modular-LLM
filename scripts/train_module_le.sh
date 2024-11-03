a1510f708497a7af8bcc2039f87af0d8589a3d7f
Ali Edalat <ali.edalat.77@gmail.com>
2024-11-03 12:52:17 +0330

    add train code (ipynb) of LE based on adamergx

--- scripts/train_module_le.sh
+++ scripts/train_module_le.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-python ../train_experts.py \
-    --language_expert=1 \
-    --language='en' \
-    --le_train_json_path='/content/en_Wiki_10k_LM_511_1.json' \
-    --le_test_json_path='/content/en_Wiki_10k_LM_511_1_test.json' \
-    --cluster_idx=0 \
-    --batch_size 2 \
-    --seed 1234
+python ../train_experts.py --language_expert=1 --language='en' --le_train_json_path='/content/en_Wiki_10k_LM_511_1.json' --le_test_json_path='/content/en_Wiki_10k_LM_511_1_test.json' --cluster_idx=0 --batch_size=2 --seed=1234
