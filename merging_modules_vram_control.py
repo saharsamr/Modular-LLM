e48b4c0daba30415e83bac5dedfd328e885b5efb
Sahar <17063634+saharsamr@users.noreply.github.com>
2024-11-10 11:38:26 +0330

    Evaluation (#5)
    * add datasets, needs test
    
    * homogenization of test datasets
    
    * test a bug
    
    * remove extra module initializations
    
    * fix minor bug
    
    * fix minor bug
    
    * add trust remote code
    
    * add print line
    
    * specify target column
    
    * fix minor bug
    
    * test
    
    * change labels to natural language
    
    * test
    
    * test
    
    * test
    
    * add input for fair comparison
    
    * test
    
    * add multichoice evaluation
    
    * add pbar total
    
    * fix number of samples for test
    
    * fix minor bug
    
    * fix minor bug
    
    * fix minor bug
    
    * add hswag dataset
    
    * fix minor bug
    
    * delete unnecessary files
    
    * refactor

--- merging_modules_vram_control.py
+++ merging_modules_vram_control.py
@@ -1,28 +1,26 @@
+import random
+
+import numpy as np
+import torch
+from tqdm import tqdm
 from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     BitsAndBytesConfig,
     pipeline,
 )
-from datasets import load_dataset
-import torch
-from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score
 
-from tqdm import tqdm
-import random
-import numpy as np
-
-from utils.arg_parser import experts_merging_arg_parser
+from data_handler.test_datasets import (
+    read_test_dataset,
+    create_multi_choice_options,
+    extract_multi_choice_target_index,
+)
+from merging_lora_modules.arrow_routing import ArrowRouting
 from merging_lora_modules.simple_averaging import SimpleAveraging
 from merging_lora_modules.xlora_average import XLoraAveraging
-from merging_lora_modules.arrow_routing import ArrowRouting
-from data_handler.dataset import (
-    apply_preprocessing,
-    create_message_column_for_test
-)
-from utils.metrics import compute_generation_metrics
+from utils.arg_parser import experts_merging_arg_parser
 from utils.config import *
-
 
 
 def set_seed(seed: int):
@@ -30,6 +28,55 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+
+def compute_loglike_loss(logits, labels, reduction="none"):
+    bs = logits.size(0)
+    vocab_size = logits.size(-1)
+    labels = labels.squeeze(-1)
+    shift_logits = logits[..., :-1, :].contiguous()
+    shift_labels = labels[..., 1:].contiguous()
+
+    # Flatten the tokens
+    loss_fct = torch.nn.CrossEntropyLoss(reduction=reduction)
+    shift_logits = shift_logits.view(-1, vocab_size)
+    shift_labels = shift_labels.view(-1)
+
+    shift_labels = shift_labels.to(shift_logits.device)
+    loss = loss_fct(shift_logits, shift_labels)
+
+    # reshape back
+    if reduction == "none":
+        loss = loss.view((bs, -1))
+        # mean only non-zero
+        non_zero_loss = (loss != 0).sum(dim=-1)
+        non_zero_loss[non_zero_loss == 0] = 1
+        loss = loss.sum(dim=-1) / non_zero_loss
+    return loss
+
+
+multi_choice_datasets = ['piqa', 'boolq', 'swag', 'hswag', 'arc-easy', 'arc-challenge']
+
+
+def evaluate_on_multi_choice(eval_dataset, model, tokenizer, ds_name, routing_strategy):
+    for i, sample in tqdm(enumerate(eval_dataset), total=len(eval_dataset)):
+        options = create_multi_choice_options(sample, ds_name)
+        option_losses = []
+        for option in options:
+            tokenized_text = tokenizer(
+                text=option, text_target=option, return_tensors='pt', truncation=True, max_length=512).to('cuda')
+            if routing_strategy == 'arrow_routing':
+                logits = model(tokenized_text['input_ids'], compute_arrow_weights=True, top_k=3).logits
+            else:
+                logits = model(tokenized_text['input_ids']).logits
+            loss = compute_loglike_loss(logits, tokenized_text['labels'])
+            option_losses.append(loss.to('cpu'))
+
+        labels.append(extract_multi_choice_target_index(sample, args.dataset_name))
+        predictions.append(np.argmin(option_losses))
+
+    print(f'Accuracy for dataset {args.dataset_name} and strategy {args.merging_strategy} is: '
+          f'{accuracy_score(labels, predictions)}')
 
 
 if __name__ == "__main__":
@@ -42,14 +89,13 @@ if __name__ == "__main__":
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     print('Loading Model ...')
     bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_use_double_quant=False,
-        )
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=False,
+    )
     model = AutoModelForCausalLM.from_pretrained(
         args.model_name, torch_dtype=torch.float16, quantization_config=bnb_config)
-    
 
     if args.merging_strategy == "simple_average":
         expert_merger = SimpleAveraging(model, tokenizer, args.model_name)
@@ -66,14 +112,8 @@ if __name__ == "__main__":
             expert_merger = XLoraAveraging(model, tokenizer, args.model_name, args.data_portion)
             expert_merger.merge()
             strategy_model = expert_merger.get_model()
-    
-    elif args.merging_strategy == 'arrow_routing':
-        # expert_merger = ArrowRouting(model, tokenizer, args.model_name)
-        # # vectors_dict, eigvals_dict = expert_merger.routing_function()
-        # expert_merger.merge(k=3)
-        # model = expert_merger.get_model()
-        # print(model)
 
+    elif args.merging_strategy == 'arrow_routing':
         # ًWe only load the model with all the adapters here, the merging will be done inside the model's layer
         expert_merger = ArrowRouting(model, tokenizer, args.model_name)
         strategy_model = expert_merger.get_model()
@@ -84,89 +124,13 @@ if __name__ == "__main__":
 
     else:
         raise f'{args.merging_strategy} is not supported.'
-    
-    pipe = pipeline(task="text-generation", model=strategy_model, tokenizer=tokenizer, truncation=True, padding=True)
 
-    routing_test_dataset = load_dataset("TahaBa/flan-routing-MoE-dataset", cache_dir="../data/")['test']
-    routing_test_dataset = routing_test_dataset if args.data_portion == 1.0 \
-        else routing_test_dataset.train_test_split(test_size=1-args.data_portion)['train']
-    routing_test_dataset = routing_test_dataset.map(create_message_column_for_test)
-    routing_test_dataset = routing_test_dataset.map(
-        lambda sample:
-        {'text': pipe.tokenizer.apply_chat_template(sample['messages'], tokenize=False, add_generation_prompt=True)}
-    )
+    routing_test_dataset = read_test_dataset(args.dataset_name)
+    routing_test_dataset = routing_test_dataset.train_test_split(test_size=200, seed=args.seed)['test']
 
-    test_dataloader = DataLoader(routing_test_dataset, batch_size=args.batch_size)
-    references, predictions = [], []
-    for i, batch in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
-        # Calling the model's forward path to apply Arrow Routing
-
-        # print(batch['text'])
-
-        tokenised_batch = tokenizer(batch['text'], return_tensors="pt", truncation=True, padding=True).to('cuda')
-        
-        print(len(tokenised_batch['input_ids'][0]))
-
-        if len(tokenised_batch['input_ids'][0]) > 1500:
-            continue
-
-        if len(references) > 20:
-            break
-
-        if args.merging_strategy == 'arrow_routing':
-            strategy_model(**tokenised_batch, compute_arrow_weights=True, top_k=3)
-        elif args.merging_strategy == 'phi3':
-            strategy_model(**tokenised_batch)
-
-        
-
-        # Generate the answer using the new adapter
-        outputs = pipe(batch['text'], max_new_tokens=100)
-        preds = [output[0]['generated_text'].split("<|assistant|>\n")[1].strip() for output in outputs]
-        print(preds)
-
-        references.extend(batch['target'])
-        predictions.extend(preds)
-
-        del strategy_model
-        del pipe
-        del expert_merger
-        del tokenizer
-        del model
-        del bnb_config
-        torch.cuda.empty_cache()
-        import gc
-        # # del your_tensor  # Replace 'your_tensor' with the variable name
-        gc.collect()
-        torch.cuda.empty_cache() # add to forward method in bnb after return
-
-        
-        tokenizer = AutoTokenizer.from_pretrained(
-            args.model_name, use_fast=True, padding_side='right', model_max_length=MAX_LENGTH
-        )
-        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-        print('Loading Model ...')
-        bnb_config = BitsAndBytesConfig(
-                load_in_4bit=True,
-                bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch.float16,
-                bnb_4bit_use_double_quant=False,
+    labels, predictions = [], []
+    with torch.no_grad():
+        if args.dataset_name in multi_choice_datasets:
+            evaluate_on_multi_choice(
+                routing_test_dataset, strategy_model, tokenizer, args.dataset_name, args.merging_strategy
             )
-        model = AutoModelForCausalLM.from_pretrained(
-            args.model_name, torch_dtype=torch.float16, quantization_config=bnb_config)
-        
-        if args.merging_strategy == 'arrow_routing':
-            expert_merger = ArrowRouting(model, tokenizer, args.model_name)
-            strategy_model = expert_merger.get_model()
-        elif args.merging_strategy == 'phi3':
-            expert_merger = None
-            strategy_model = model
-
-        pipe = pipeline(task="text-generation", model=strategy_model, tokenizer=tokenizer, truncation=True, padding=True)
-
-    metrics = compute_generation_metrics(references, predictions)
-    print('=' * 100)
-    print('BLEU:', metrics['bleu'])
-    print('ROUGE:', metrics['rouge'])
-    print('BERTSCORE:', metrics['bertscore'])
-    print('=' * 100)
