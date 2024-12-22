52d0b70ea874529ea6216dbbb32af19622bbe0fa
saaharsamr <sahar.rajabi76@gmail.com>
2024-12-22 16:22:25 +0000

    change target modules

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
 MAX_LENGTH = 4000
 
-LORA_TARGET_MODULES = ["o_proj", "qkv_proj"]
+# LORA_TARGET_MODU LES = ["o_proj", "qkv_proj"]
+LORA_TARGET_MODULES = ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"]
 # OTHER_TRAINABLE_MODULES = ['embed_tokens', 'lm_head']
 TASK_TYPE = "CAUSAL_LM"
 
