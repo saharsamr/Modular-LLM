921fa9b4a3982fdb6f34833aef36d48ab89b222f
saaharsamr <sahar.rajabi76@gmail.com>
2024-12-22 16:10:09 +0000

    expert path

--- utils/config.py
+++ utils/config.py
@@ -4,4 +4,4 @@ LORA_TARGET_MODULES = ["o_proj", "qkv_proj"]
 # OTHER_TRAINABLE_MODULES = ['embed_tokens', 'lm_head']
 TASK_TYPE = "CAUSAL_LM"
 
-EXPERTS_FOLDER_PATH = 'results'
+EXPERTS_FOLDER_PATH = 'results/llama'
